f8e02f02657acb14f569922775d70d3bfb104bd2
gavinesajas <gavnesajas@gmail.com>
2017-04-15 23:43:55 +0200

    Initial Commit

---
+++ apiTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apiTest
+//
+//  Created by Gavin Esajas on 15/04/2017.
+//  Copyright © 2017 Gavin Esajas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
